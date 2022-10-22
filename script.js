9155831837456c653f5ff1fdd7603380e286f9b5
afreedi <afreedisulfiker@gmai.com>
2022-10-22 21:22:32 +0530

    full completed

--- script.js
+++ script.js
@@ -4,7 +4,27 @@ $(document).ready(function(){
             $('.navbar').addClass("sticky");
         }else{
             $('.navbar').removeClass("sticky");
+        }if(this.scroll>500){
+            $('.scroll-up-btn').addClass("show");
+        }else{
+            $('.scroll-up-btn').removeClass("show");
         }
+    });
+    $(".scroll-up-btn").click(function(){
+        $('html').animate({scrollTop:0})
+    })
+    //typing animation
+    var typed = new Typed(".typing",{
+        strings:["Youtuber","Developer","Blogger","Designer","Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
+    })
+    var typed = new Typed(".typing-2",{
+        strings:["Youtuber","Developer","Blogger","Designer","Freelancer"],
+        typeSpeed:100,
+        backSpeed:60,
+        loop:true
     })
     // toggle menubar/navbar script
     $('.menu-btn').click(function(){
